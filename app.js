4d13f3027fb5e46102b97d4d246eee31ec781397
Alberto Miranda Dencowski <allowski@gmail.com>
2020-04-25 22:55:08 -0300

    Added first files

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const {createServer} = require('http');
 const app = require('express')();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const server = createServer(app);
 
 const io = require('socket.io')(server);
+
+app.use(cors());
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send({hello: 'world'})
@@ -11,6 +17,25 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('connected');
+    socket.on('subscribe', room => {
+       socket.join(room);
+       io.to(room).emit('entered');
+    });
+    socket.on('unsubscribe', room => {
+        socket.join(room);
+        io.to(room).emit('left');
+    });
+});
+
+app.post('/emit/:room/:message', (req, res) => {
+    console.log({
+        room: req.params.room,
+        event: req.params.message,
+    })
+   io.to(req.params.room).emit(req.params.message, req.body)
+   res.send({
+      test: true
+   });
 });
 
 server.listen(3000);
